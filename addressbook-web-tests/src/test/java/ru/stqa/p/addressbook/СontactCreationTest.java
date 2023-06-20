890009e1dbff853b65e025239da64ad7672b8415
1242133 <135114271+1242133@users.noreply.github.com>
2023-06-20 15:31:41 +0300

    Тест для Контактов на заполнение основных полей

--- addressbook-web-tests/src/test/java/ru/stqa/p/addressbook/СontactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/p/addressbook/СontactCreationTest.java
@@ -20,27 +20,51 @@ public class СontactCreationTest {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.id("LoginForm")).submit();
   }
 
   @Test
   public void testСontactCreation() throws Exception {
-    wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Alexey");
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Orlov");
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("89445556767");
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("a123@gmail.com");
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    wd.findElement(By.linkText("home page")).click();
+    gotoContactsPage();
+    initContactsCreation();
+    fillContactsFrom(new GroupContact("Alexey", "Orlov", "89445556767", "a123@gmail.com"));
+    submitContactsCreation();
+    returnToContactsPage();
+  }
+
+  private void returnToContactsPage() {
     wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitContactsCreation() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void fillContactsFrom(GroupContact groupContact) {
+    wd.findElement(By.name("firstname")).sendKeys(groupContact.firstname());
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(groupContact.lastname());
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(groupContact.mobile());
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(groupContact.email());
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void initContactsCreation() {
+    wd.findElement(By.name("firstname")).clear();
+  }
+
+  private void gotoContactsPage() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
