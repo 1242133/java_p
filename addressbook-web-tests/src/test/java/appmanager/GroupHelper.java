7e2c15fca8e8edeaa00f622dcd53750fa233b373
1242133 <135114271+1242133@users.noreply.github.com>
2023-06-29 15:40:10 +0300

    Создали общий класс для классов помошников

--- addressbook-web-tests/src/test/java/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/appmanager/GroupHelper.java
@@ -4,43 +4,35 @@ import model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class GroupHelper {
-  private WebDriver wd;
+public class GroupHelper extends HelperBase {
 
   public GroupHelper(WebDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void returnToGroupPage() {
-    wd.findElement(By.linkText("Logout")).click();
+    click(By.linkText("group page"));
   }
 
   public void submitGroupCreation() {
-    wd.findElement(By.linkText("group page")).click();
+    click(By.name("submit"));
   }
 
   public void fillGroupFrom(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.name());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.header());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.footer());
-    wd.findElement(By.name("submit")).click();
+    type(By.name("group_name"), groupData.name());
+    type(By.name("group_header"), groupData.header());
+    type(By.name("group_footer"), groupData.footer());
   }
 
   public void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
+    click(By.name("new"));
   }
 
   public void deleteSelectedGroups() {
-    wd.findElement(By.name("delete")).click();
+    click(By.name("delete"));
   }
 
   public void selectGroup() {
-    wd.findElement(By.name("selected[]")).click();
+    click(By.name("selected[]"));
   }
 }
