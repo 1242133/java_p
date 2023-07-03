a8fd15c8d1a3af8057725341769da7aeea704a37
1242133 <135114271+1242133@users.noreply.github.com>
2023-07-03 14:44:49 +0300

    исправление getNavigationHelper().gotoGroupPage();

--- addressbook-web-tests/src/test/java/homework/СontactCreationTest.java
+++ addressbook-web-tests/src/test/java/homework/СontactCreationTest.java
@@ -5,7 +5,7 @@ import tests.TestBase;
 public class СontactCreationTest extends TestBase {
   @Test
   public void testСontactCreation() throws Exception {
-    App.getNavigationHelper().gotoGroupPage();
+    App.getNavigationHelper().gotoContactsPage();
     App.getGroupHelper().initContactsCreation();
     App.getGroupHelper().fillContactsFrom(new ContactData("Alexey", "Orlov", "89445556767", "a123@gmail.com"));
     App.getGroupHelper().submitContactsCreation();
