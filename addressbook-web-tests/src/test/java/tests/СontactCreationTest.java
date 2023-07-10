4147e75967aec6d5b9ad20890b86f68bc751ea3f
1242133 <135114271+1242133@users.noreply.github.com>
2023-07-10 13:01:39 +0300

    Оптимизированы переходы между станицами

--- addressbook-web-tests/src/test/java/tests/СontactCreationTest.java
+++ addressbook-web-tests/src/test/java/tests/СontactCreationTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class СontactCreationTest extends TestBase {
   @Test
   public void testСontactCreation() throws Exception {
-    app.getNavigationHelper().gotoContactsPage();
+    app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().initContactsCreation();
     app.getContactHelper().fillContactsFrom(new ContactData("Al", "Orlov", "89445556767", "a123@gmail.com", "test1"), true);
     app.getContactHelper().submitContactsCreation();
