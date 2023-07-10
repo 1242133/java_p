20beb44746fa6ee8086c546f4dc46b82485db18d
1242133 <135114271+1242133@users.noreply.github.com>
2023-07-10 14:38:05 +0300

    Реализована проверка и обеспечения выполнения предусловий

--- addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import model.GroupData;
 import org.testng.annotations.Test;
 
 
@@ -9,6 +10,9 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
