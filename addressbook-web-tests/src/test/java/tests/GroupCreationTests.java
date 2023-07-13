0b037aa5a1809ccacb134e0573412c9062843a8a
1242133 <135114271+1242133@users.noreply.github.com>
2023-07-13 14:43:05 +0300

    контроль количество элементов для групп

--- addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
@@ -1,14 +1,18 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import model.GroupData;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
-  public void testGroupCreation() throws Exception {
+  public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before + 1);
   }
 
 }
--- addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -10,12 +11,15 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before -1);
   }
 
 }
