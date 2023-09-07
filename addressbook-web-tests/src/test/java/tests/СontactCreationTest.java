3371bf147f47b51c58d5f11338502f111f9a9bca
1242133 <135114271+1242133@users.noreply.github.com>
2023-09-07 15:46:52 +0300

    сохранение данных в формате xml и  загрузка тестовых данных для контактов(не закончил)

--- addressbook-web-tests/src/test/java/tests/СontactCreationTest.java
+++ addressbook-web-tests/src/test/java/tests/СontactCreationTest.java
@@ -2,22 +2,40 @@ package tests;
 
 import model.ContactData;
 import model.Contacts;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class СontactCreationTest extends TestBase {
 
-  @Test
-  public void testСontactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withEmail(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testСontactCreation(ContactData contact) {
     app.goTo().contactPage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().withFirstname("Alexey").withLastname("Orlov").withMobile(null).withEmail(null).withGroup(null).withPhoto(photo);
+    //File photo = new File("src/test/resources/stru.png");
     app.contact().create(contact);
     Contacts after = app.contact().all();
     assertThat(after.size(), equalTo(before.size() + 1));
